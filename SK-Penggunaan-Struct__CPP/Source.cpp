d301772a3714fb86d9fa8c5cb8d37b4225e4dcbd
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-01-23 17:13:08 +0700

    Add project files.

---
+++ SK-Penggunaan-Struct__CPP/Source.cpp
@@ -0,0 +1,30 @@
+
+#include<conio.h>
+#include<iostream>
+
+using namespace std;
+
+/* Created by Rizky Khapidsyah */
+
+int main() {
+	struct {
+		char nim[5];
+		char nama[15];
+		int nilai;
+	} mahasiswa;
+	
+	cout << "masukan NIM = ";
+	cin >> mahasiswa.nim;
+	cout << "masukan Nama = ";
+	cin >> mahasiswa.nama;
+	cout << "masukan Nilai Akhir = ";
+	cin >> mahasiswa.nilai;
+
+	cout << "\n\nData Yang di Inputkan adalah : \n\n";
+	cout << "NIM = " << mahasiswa.nim << endl;
+	cout << "Nama = " << mahasiswa.nama << endl;
+	cout << "Nilai Akhir = " << mahasiswa.nilai << endl;
+	
+	_getch();
+	return 0;
+}
